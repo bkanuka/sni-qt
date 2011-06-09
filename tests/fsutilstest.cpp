d97bf2ccc4e5823b6bbe0bf0816cd879e684efa5
Aurelien Gateau <aurelien.gateau@canonical.com>
2011-06-09 16:25:31 +0200

    Relicense to GPLv3

--- tests/fsutilstest.cpp
+++ tests/fsutilstest.cpp
@@ -3,16 +3,15 @@
    Author: Aurelien Gateau <aurelien.gateau@canonical.com>
  
    sni-qt is free software; you can redistribute it and/or modify it under the
-   terms of the GNU Lesser General Public License (LGPL) as published by the
-   Free Software Foundation; either version 2.1 of the License, or (at your
-   option) any later version.
+   terms of the GNU General Public License (GPL) as published by the Free
+   Software Foundation; version 3 of the License.
 
    sni-qt is distributed in the hope that it will be useful, but WITHOUT ANY
-   WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-   A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+   WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+   FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
    details.
 
-   You should have received a copy of the GNU Lesser General Public License
+   You should have received a copy of the GNU General Public License
    along with sni-qt.  If not, see <http://www.gnu.org/licenses/>.
 */
 // Local
