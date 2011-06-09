58047b475043cb623d4e0761564fb828d7c8f210
Aurelien Gateau <aurelien.gateau@canonical.com>
2011-06-09 14:52:46 +0200

    Fixed copyright headers

--- src/dbustooltip.cpp
+++ src/dbustooltip.cpp
@@ -1,22 +1,19 @@
-/* This file is part of the appmenu-qt project
+/* This file is part of the sni-qt project
    Copyright 2011 Canonical
    Author: Aurelien Gateau <aurelien.gateau@canonical.com>
+ 
+   sni-qt is free software; you can redistribute it and/or modify it under the
+   terms of the GNU Lesser General Public License (LGPL) as published by the
+   Free Software Foundation; either version 2.1 of the License, or (at your
+   option) any later version.
 
-   This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Library General Public
-   License (LGPL) as published by the Free Software Foundation;
-   either version 2 of the License, or (at your option) any later
-   version.
+   sni-qt is distributed in the hope that it will be useful, but WITHOUT ANY
+   WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+   A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+   details.
 
-   This library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Library General Public License for more details.
-
-   You should have received a copy of the GNU Library General Public License
-   along with this library; see the file COPYING.LIB.  If not, write to
-   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-   Boston, MA 02110-1301, USA.
+   You should have received a copy of the GNU Lesser General Public License
+   along with sni-qt.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include <dbustooltip.h>
 
